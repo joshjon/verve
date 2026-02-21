e7053f8ab176c30ff080c0bff6d277efd0045075
Verve Agent <>
2026-02-21 11:06:29 +0000

    wip: Replace custom stop icon with lucide StopCircle component

--- ui/src/routes/tasks/[id]/+page.svelte
+++ ui/src/routes/tasks/[id]/+page.svelte
@@ -1417,10 +1417,7 @@
 									{#if stopping}
 										<Loader2 class="w-4 h-4 animate-spin" />
 									{:else}
-										<svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-											<circle cx="12" cy="12" r="10" />
-											<rect x="6" y="6" width="12" height="12" fill="currentColor" />
-										</svg>
+										<StopCircle class="w-4 h-4" />
 									{/if}
 									Stop Agent
 								</Button>
